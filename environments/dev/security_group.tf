08f851884afb7b4758b8c3b3533ab351036d318f
Rahulrexjha <federerqaz@gmail.com>
2025-09-18 19:03:47 +0530

    Update security_group.tf

--- environments/dev/security_group.tf
+++ environments/dev/security_group.tf
@@ -6,6 +6,6 @@ module "security_group" {
   vpc_id      = module.vpc.vpc_id
   depends_on  = [ module.vpc ]
 
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  ingress_rules       = ["http-80-tcp","ssh-tcp"]
+   ingress_cidr_blocks = ["0.0.0.0/0"]
+   ingress_rules       = ["http-80-tcp","ssh-tcp"]
 }
